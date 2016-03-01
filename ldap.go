2a8bb6cefd07109f9958b84d61143abecff4fc1a
Fred Yankowski <fcy@imsa.edu>
2016-03-01 16:07:10 -0600

    Include OU information with generated data.

--- ldap.go
+++ ldap.go
@@ -6,6 +6,9 @@ import (
 	"crypto/tls"
 	"fmt"
 	"gopkg.in/ldap.v2"
+	"log"
+	"regexp"
+	"strings"
 )
 
 type Person struct {
@@ -14,6 +17,7 @@ type Person struct {
 	FirstName string
 	LastName  string
 	OrgStatus string
+	Ous       []string
 }
 
 func Search(ldapAddress, pattern string) (chan Person, error) {
@@ -49,19 +53,60 @@ func Search(ldapAddress, pattern string) (chan Person, error) {
 	go func() {
 		defer close(persons)
 
+	entries:
 		for _, entry := range sr.Entries {
 			//entry.Print()
+
+			ous := ousFromDn(entry.DN)
+			for _, ou := range ous {
+				ou = strings.ToLower(ou)
+				if ou == "faxes" || ou == "machines" {
+					continue entries
+				}
+			}
+
 			person := Person{}
 			person.Dn = entry.DN
+
 			// Save only one value for most attributes since that's all we store.
 			person.FirstName = entry.GetAttributeValue("givenName")
 			person.LastName = entry.GetAttributeValue("sn")
 			person.OrgStatus = entry.GetAttributeValue("organizationalStatus")
+
 			// We do use multiple uid values for uid (for alumni).
 			person.Uid = entry.GetAttributeValues("uid")
+
+			person.Ous = ous
+
 			persons <- person
 		}
 	}()
 
 	return persons, nil
+}
+
+var uninterestingPattern = regexp.MustCompile("(?i:,ou=(faxes|machines),)")
+
+// uninteresting() returns true iff the DN indicates an entry that we want to
+// ignore. This is a hack because DN values are supposed to be opaque.
+func uninteresting(dn string) bool {
+	return uninterestingPattern.Match([]byte(dn))
+}
+
+func ousFromDn(dn string) []string {
+	parts := strings.Split(dn, ",")
+	var ous = []string{}
+	for _, part := range parts {
+		nameVal := strings.Split(part, "=")
+		if len(nameVal) != 2 {
+			log.Printf("error: bad DN part: %v", part)
+			continue
+		}
+		if nameVal[0] == "ou" {
+			if nameVal[1] != "People" {
+				ous = append(ous, nameVal[1])
+			}
+		}
+	}
+	return ous
 }
